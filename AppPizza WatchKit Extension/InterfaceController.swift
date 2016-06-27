4bbf25e0918fb5e360849ba5ae4a4cfff6dd4347
Roberto Carlos Callisaya Mamani <callisaya@p2ntpercallisaya-5589.nuevatel.com>
2016-06-27 09:47:36 -0400

    Initial Commit

---
+++ AppPizza WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  AppPizza WatchKit Extension
+//
+//  Created by Roberto Carlos Callisaya Mamani on 6/27/16.
+//  Copyright © 2016 Roberto Carlos Callisaya Mamani. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
